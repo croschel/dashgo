75fd7d81e16d165fd007c945eb8b3101f255f546
Caique Roschel <croschel000@gmail.com>
2021-05-10 16:35:17 -0300

    Sidebar Componentization

---
+++ src/components/Sidebar/NavLink.tsx
@@ -0,0 +1,19 @@
+import { Icon, Link, LinkProps, Text } from "@chakra-ui/react";
+import { ElementType } from "react";
+import { RiInputMethodLine } from "react-icons/ri";
+
+interface NavLinkProps extends LinkProps {
+  icon: ElementType;
+  children: string;
+}
+
+export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+  return (
+    <Link display="flex" align="center" {...rest}>
+      <Icon as={icon} fontSize="20" />
+      <Text ml="4" fontWeight="medium">
+        {children}
+      </Text>
+    </Link>
+  );
+}
